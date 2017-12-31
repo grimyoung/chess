7e47a57226f42531b0afc8f94170de6f411bda58
grimyoung <samhyukim@gmail.com>
2017-12-30 17:57:25 -0800

    Rook moves

--- board.rb
+++ board.rb
@@ -91,7 +91,7 @@ module Chess
       when "N"
         moves = knight_moves(color,pos)
       when "R"
-
+        moves = rook_moves(color,pos)
       when "B"
 
       when "Q"
@@ -163,6 +163,30 @@ module Chess
       return moves
     end
 
+    #todo castling or  castling is a king move?
+    def rook_moves(color,pos)
+      x,y = pos
+      moves = []
+      possible = [[1,0], [-1,0], [0,1],[0,-1]]
+      possible.each do |adj|
+        a,b = x + adj[0], y + adj[1]
+        pos = [a,b]
+        blocked = false
+        while(in_bounds?(pos) && !blocked)
+          if square_empty?(pos)
+            moves.push(pos)
+          elsif enemy_square?(color,pos)
+            moves.push(pos)
+            blocked = true
+          else
+            blocked = true
+          end
+          a,b = pos[0] + adj[0], pos[1] + adj[1]
+          pos = [a,b]
+        end
+      end
+      return moves
+    end
     
   end
 end
@@ -170,5 +194,6 @@ end
 test = Chess::Board.new
 test.display_grid
 puts
-p test.valid_moves?("N","w", [2,1])
+p test.in_bounds?([100,0])
+p test.valid_moves?("R","w", [2,1])
 #test.display_grid
