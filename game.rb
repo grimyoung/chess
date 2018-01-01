0a309d8fc01dd092914e7e6b63f41199a5ef38d7
grimyoung <samhyukim@gmail.com>
2017-12-31 19:19:29 -0800

    parts of stalemate method

--- game.rb
+++ game.rb
@@ -3,15 +3,18 @@ require_relative "piece.rb"
 
 module Chess
   class Game
-    attr_accessor :board, :turn, :current_player
+    attr_accessor :board, :turn, :current_player, :last_captured_or_pawn_moved, :draw_proposal
     def initialize
-      #board = Board.new
+      board = Board.new
       turn = 0
       current_player = "w"
       last_captured_or_pawn_moved = 0
+      draw_proposal = 0
     end
 
     def play
+      
+
     end
 
     def in_check?(board_state)
@@ -27,6 +30,23 @@ module Chess
       return false
     end
 
+    def check_mate?(board_state)
+      
+    end
+
+    def stalemate?(board_state)
+      if turn - last_captured_or_pawn_moved >= 50
+        puts "Game over - Stalemate: Over fifty moves without piece capture or pawn moved"
+      end
+      if draw_proposal == true
+        puts "Game over- Stalemate: Drawn by agreement"
+      end
+      #no legal moves but not in check
+      #Threefold Repetition
+      #Insufficient Mating Material
+    end
+
+
     def change_player
       if current_player == "w"
         current_player = "b"
@@ -34,6 +54,8 @@ module Chess
         current_player = "w"
       end
     end
+
+
   end
 end
 
