c6ce6c4c64914721888cc3b66680bd84ea196c68
grimyoung <samhyukim@gmail.com>
2017-12-31 16:41:36 -0800

    added some game logic

--- game.rb
+++ game.rb
@@ -3,7 +3,37 @@ Dir[lib_path + "/**/*.rb"].each {|file| require file}
 
 module Chess
   class Game
-    
+    attr_accessor :board, :turn, :current_player
+    def initialize
+      board = Board.new
+      turn = 0
+      current_player = "w"
+    end
+
+    def play
+    end
+
+    def in_check?(board_state)
+      if current_player == "w"
+        if board_state.black_attack.include?board_state.king_position[0]
+          return true
+        end
+      else
+        if board_state.white_attack.include?board_state.king_position[1]
+          return true
+        end
+      end
+      return false
+    end
+
+    def change_player
+      if current_player == "w"
+        current_player = "b"
+      else
+        current_player = "w"
+      end
+    end
+
   end
 end
 
