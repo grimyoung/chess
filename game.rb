9e9b4638f68bd4ec28fcf618004af1024a088015
grimyoung <samhyukim@gmail.com>
2018-01-01 21:29:48 -0800

    testing

--- game.rb
+++ game.rb
@@ -13,8 +13,19 @@ module Chess
     end
 
     def play
-      
-
+      intro
+      while game_over?(board)
+        puts player_color(current_player) + " please make a move"
+        user_move = gets.chomp
+        while ! valid_input(user_move)
+          puts "That was not a valid input."
+          puts "Please try again: "
+          user_move = gets.chomp
+        end
+        #now the input is valid 
+        #check if input is a legal move
+        
+      end
     end
 
     def in_check?(board_state)
@@ -31,21 +42,37 @@ module Chess
     end
 
     def check_mate?(board_state)
-      
+      #current player is in check and 0 legal_moves
+    end
+
+    def legal_moves(current_player)
+      #hash with object + moves
     end
 
     def stalemate?(board_state)
       if turn - last_captured_or_pawn_moved >= 50
         puts "Game over - Stalemate: Over fifty moves without piece capture or pawn moved"
+        return true
       end
       if draw_proposal == true
         puts "Game over- Stalemate: Drawn by agreement"
+        return true
       end
       #no legal moves but not in check
       #Threefold Repetition
       #Insufficient Mating Material
+      return false
     end
 
+    def game_over?(board_state)
+      if check_mate?(board_state)
+        puts "Game_over by check_mate!"
+        return true
+      elsif stalemate?(board_state)
+        return true
+      end
+      return false
+    end
 
     def change_player
       if current_player == "w"
@@ -55,11 +82,40 @@ module Chess
       end
     end
 
+    def intro
+      puts "Welcome to chess"
+      puts "Moves are in the form letternumber e.g. a1"
+    end
 
+    def player_color(text)
+      if text == "w"
+        return "white"
+      end
+      return "black"
+    end
+
+    def valid_input(string)
+      if string.length == 2 && string[0] =~ /[[:alpha:]]/ && string[1] =~ /[[:digit:]]/
+        return true
+      end
+      return false
+    end
   end
 end
 
 test = Chess::Board.new
 test.display_grid
-test.attacked_squares
-test.inspect
+# test.remove_piece("f8")
+# test.remove_piece("g8")
+# test.remove_piece("f1")
+# test.remove_piece("g1")
+
+#test.update_board
+test.move_piece("e2", "e4")
+p test.enpassant
+p test.enpassant_file
+#test.attacked_squares
+# puts test.ks_castle_possible?("w")
+test.display_grid
+#test.attacked_squares
+#test.inspect
