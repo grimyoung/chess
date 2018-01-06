99fccdb3deca104b1fd21a6f02d505d1f4a247a3
grimyoung <samhyukim@gmail.com>
2018-01-05 17:48:02 -0800

    finished up game logic

--- game.rb
+++ game.rb
@@ -14,41 +14,30 @@ module Chess
 
     def play
       intro
-      while game_over?(board)
+      while !game_over?(board.game_over?(current_player))
+        board.update_board
+        board.display_grid
+        player_check(board)
         puts player_color(current_player) + " please make a move"
-        user_move = gets.chomp
-        while ! valid_input(user_move)
-          puts "That was not a valid input."
-          puts "Please try again: "
-          user_move = gets.chomp
-        end
-        #now the input is valid 
+        move = get_user_input 
         #check if input is a legal move
-        
+        while !board.legal_move?(current_player, move[0],move[1])
+          puts "That was not a legal move for that piece"
+          puts "Try again"
+          move = get_user_input
+        end
+        board.move_piece[move[0],move[1]]
+        change_player
       end
     end
 
-    def in_check?(board_state)
-      if current_player == "w"
-        if board_state.black_attack.include?board_state.king_position[0]
-          return true
-        end
-      else
-        if board_state.white_attack.include?board_state.king_position[1]
-          return true
-        end
+    def player_check(board_state)
+      if board_state.king_checked?(current_player)
+        puts player_color(current_player) + "is in check!"
       end
-      return false
     end
 
-    def check_mate?(board_state)
-      #current player is in check and 0 legal_moves
-    end
-
-    def legal_moves(current_player)
-      #hash with object + moves
-    end
-
+    #defined in board.rb
     def stalemate?(board_state)
       if turn - last_captured_or_pawn_moved >= 50
         puts "Game over - Stalemate: Over fifty moves without piece capture or pawn moved"
@@ -64,15 +53,6 @@ module Chess
       return false
     end
 
-    def game_over?(board_state)
-      if check_mate?(board_state)
-        puts "Game_over by check_mate!"
-        return true
-      elsif stalemate?(board_state)
-        return true
-      end
-      return false
-    end
 
     def change_player
       if current_player == "w"
@@ -100,25 +80,27 @@ module Chess
       end
       return false
     end
+
+    def valid_loop
+      user_move = gets.chomp
+      while ! valid_input(user_move)
+        puts "That was not a valid input."
+        puts "Please try again: "
+        user_move = gets.chomp
+      end
+      return user_move
+    end
+
+    def get_user_input
+      puts "Starting square:"
+      start_square = valid_loop
+      puts "Ending square:"
+      end_square = valid_loop
+      return [start_square,end_square]
+    end
+
   end
 end
 
-test = Chess::Board.new
-test.display_grid
-# test.remove_piece("f8")
-# test.remove_piece("g8")
-# test.remove_piece("f1")
-# test.remove_piece("g1")
-
-#test.update_board
-test.move_piece("d7", "d5")
-test.move_piece("f1", "b5")
-test.move_piece("b5", "d7")
-#test.move_piece("d8", "d7")
-test.update_board("w")
-p test.grid[1][3].path_to_king
-#test.attacked_squares
-# puts test.ks_castle_possible?("w")
-test.display_grid
-#test.attacked_squares
-#test.inspect
+test = Chess::Game.new
+test.play
