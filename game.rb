99c90ec45c50f0724953ae570e570117404ca84f
grimyoung <samhyukim@gmail.com>
2018-01-05 18:50:33 -0800

    bugs

--- game.rb
+++ game.rb
@@ -5,28 +5,32 @@ module Chess
   class Game
     attr_accessor :board, :turn, :current_player, :last_captured_or_pawn_moved, :draw_proposal
     def initialize
-      board = Board.new
-      turn = 0
-      current_player = "w"
-      last_captured_or_pawn_moved = 0
-      draw_proposal = 0
+      @board = Board.new
+      @turn = 0
+      @current_player = "w"
+      @last_captured_or_pawn_moved = 0
+      @draw_proposal = 0
     end
 
     def play
       intro
-      while !game_over?(board.game_over?(current_player))
-        board.update_board
+      board.update_board(current_player)
+      while !board.game_over?(current_player)
+        board.update_board(current_player)
         board.display_grid
+        puts
         player_check(board)
         puts player_color(current_player) + " please make a move"
         move = get_user_input 
         #check if input is a legal move
         while !board.legal_move?(current_player, move[0],move[1])
+          board.display_grid
           puts "That was not a legal move for that piece"
           puts "Try again"
+          puts
           move = get_user_input
         end
-        board.move_piece[move[0],move[1]]
+        board.move_piece(move[0],move[1])
         change_player
       end
     end
@@ -55,10 +59,10 @@ module Chess
 
 
     def change_player
-      if current_player == "w"
-        current_player = "b"
+      if self.current_player == "w"
+        self.current_player = "b"
       else
-        current_player = "w"
+        self.current_player = "w"
       end
     end
 
@@ -81,26 +85,26 @@ module Chess
       return false
     end
 
-    def valid_loop
+    def valid_loop(string)
+      puts string
       user_move = gets.chomp
       while ! valid_input(user_move)
+        board.display_grid
         puts "That was not a valid input."
-        puts "Please try again: "
+        puts "Please try again:"
+        puts string
         user_move = gets.chomp
       end
       return user_move
     end
 
     def get_user_input
-      puts "Starting square:"
-      start_square = valid_loop
-      puts "Ending square:"
-      end_square = valid_loop
+      start_square = valid_loop("Starting square:")
+      end_square = valid_loop("Ending square:")
       return [start_square,end_square]
     end
 
   end
 end
 
-test = Chess::Game.new
-test.play
+test = Chess::Game.new.play
